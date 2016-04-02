d222a4dd3159fa678e19ebdea72693aaeb0f14bb
Vishal Ranjan <vishivish18@gmail.com>
2016-04-03 01:43:53 +0530

    extraction

---
+++ public/app/controllers/homeCtrl.js
@@ -0,0 +1,15 @@
+angular.module('app')
+    .controller('homeCtrl', function($scope, $http) {
+
+
+        $scope.setup = function() {
+
+            
+
+        }
+
+        $scope.setup();
+
+
+
+    })
